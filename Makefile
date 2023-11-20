29483a203d9b6f40baf24788d710af73154e6216
Haronzs <im@keyth.me>
2023-11-20 23:19:54 +0100

    add libs

---
+++ Makefile
@@ -0,0 +1,28 @@
+#file makefile
+device := atmega328p
+cpufreq := 16000000
+port := /dev/ttyACM0
+LDFLAGS := 
+
+CC := avr-gcc
+CFLAGS := -Wall -Oz -I inc/ -DF_CPU=$(cpufreq) -mmcu=$(device)
+OBJ := main.o i2c.o
+
+CLEAN_TARGET := *.o *.bin *.hex
+
+all: project upload clean
+
+project: $(OBJ)
+	$(CC) -o $@ $^
+	avr-gcc $(LDFLAGS) -mmcu=$(device) -o main.bin $(OBJ)
+	avr-objcopy -O ihex -R .eeprom main.bin main.hex
+
+
+%.o: src/%.c
+	$(CC) $(CFLAGS) -c $<
+
+clean :
+	rm -rf $(CLEAN_TARGET)
+
+upload :
+	avrdude -F -V -c avrisp -p $(device) -P $(port) -b 115200 -U flash:w:main.hex
---
+++ src/i2c.c
@@ -0,0 +1,54 @@
+#include "i2c.h"
+
+#define TW_SDA_PIN PC4
+#define TW_SCL_PIN PC5
+
+void i2c_init()
+{
+  // Enable pullup
+  DDRC  |= (1 << TW_SDA_PIN) | (1 << TW_SCL_PIN);
+  PORTC |= (1 << TW_SDA_PIN) | (1 << TW_SCL_PIN);
+  
+  // Setup bitrate register to desired scl frequency
+  TWBR = TWBR_VALUE;
+
+  // Set prescaler to 1
+  TWSR = (0 << TWPS1) | (0 << TWPS0);
+
+  // Enable i2c lines
+  TWCR = (1 << TWEN) | (1 << TWSTO) | (0 << TWEA);
+}
+
+void i2c_close()
+{
+  // Clear the i2c control register
+  TWCR = 0;
+}
+
+void i2c_start()
+{
+  // Start condition
+  TWCR |= (1 << TWSTA) | (1 << TWEN) | (1 << TWINT);
+  while (!(TWCR & (1<<TWINT)));
+}
+
+void i2c_stop()
+{
+  // Stop condition
+  TWCR |= (1 << TWSTO) | (1 << TWEN) | (1 << TWINT);
+}
+
+void i2c_write(uint8_t data)
+{
+  // Write byte to the i2c data register
+  TWDR = data;
+  TWCR = (1 << TWINT) | (1 << TWEN);
+  while (!(TWCR & (1<<TWINT)));
+}
+
+int8_t i2c_read()
+{
+  TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+  while (!(TWCR & (1<<TWINT)));
+  return TWDR;
+}
